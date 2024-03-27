6b97a0c2c57fa881639c263f8da0cecdcdcbdee4
Nico <nicolasaddamo1@gmail.com>
2024-03-27 15:28:53 -0300

    first commit

---
+++ front/vite-project/src/view/Register.jsx/Register.jsx
@@ -0,0 +1,15 @@
+const Register = () => {
+    const [form, setForm] = useState({
+        
+        name: "",
+        lastname:"",
+        email: "",
+        password: ""
+    })
+    const [errors, setErrors] = useState({
+        name: "",
+        apellido: "",
+        email: "",
+        password: ""
+    });
+}
